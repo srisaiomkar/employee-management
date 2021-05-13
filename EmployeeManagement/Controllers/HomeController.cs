1bb727d5eb0edcea5af9ee1986596b00224f2de0
Omkar Akilla <srisaiomkar@gmail.com>
2021-05-13 22:03:29 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        public ViewResult Details()
+        {
+            Employee emp = _employeeRepository.GetEmployee(1);
+            ViewData["Employee"] = emp;
+            ViewData["PageTitle"] = "Employee Details";
+            return View();
+        }
+
+    }
+}
---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>
+            {
+                new Employee(){Id = 1, Name = "Huskar", Email = "huskar@dota.com",Department = "Carry"},
+                new Employee(){Id = 2, Name = "Anti Mage", Email = "am@dota.com",Department = "Carry"},
+                new Employee(){Id = 3, Name = "Dazzle", Email = "dazzle@dota.com",Department = "Support"},
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
