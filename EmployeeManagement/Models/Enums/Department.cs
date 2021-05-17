f72afd6a3d8f8fc08a2a234f86cf22517d51c359
Omkar Akilla <srisaiomkar@gmail.com>
2021-05-17 21:48:20 +0530

    add Employee

---
+++ EmployeeManagement/Models/Enums/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models.Enums
+{
+    public enum Department
+    {
+        IT,
+        HR,
+        Others
+    }
+}
