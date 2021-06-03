8c41c9c0c3ce990ade573872b9f9fbf0e1d9aa81
srisaiomkar <srisaiomkar@gmail.com>
2021-06-03 19:56:13 +0530

    edit role

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagement.ViewModels;
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,13 @@ namespace EmployeeManagement.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+            UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -51,6 +55,64 @@ namespace EmployeeManagement.Controllers
         {
             var roles = _roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with {id} not found";
+                return View("NotFound");
+            }
+            else
+            {
+                EditRoleViewModel model = new EditRoleViewModel
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+
+                var appUsers = await _userManager.GetUsersInRoleAsync(role.Name);
+                model.UserNames = new List<string>();
+                foreach(var user in appUsers)
+                {
+                    model.UserNames.Add(user.UserName);
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.Id);
+                if (role == null)
+                {
+                    ViewBag.ErrorMessage = $"Role with {model.Id} not found";
+                    return View("NotFound");
+                }
+                else
+                {
+                    role.Name = model.Name;
+                    var result = await _roleManager.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ListRoles");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
