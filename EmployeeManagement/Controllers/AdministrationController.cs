18be5d785cdb06b0a85fa973573c5d70a72a7d82
srisaiomkar <srisaiomkar@gmail.com>
2021-06-05 19:10:27 +0530

    delete role

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -258,5 +258,29 @@ namespace EmployeeManagement.Controllers
                 return RedirectToAction("ListUsers");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with {id} not found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return RedirectToAction("ListRoles");
+            }
+        }
     }
 }
