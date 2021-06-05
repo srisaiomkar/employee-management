a85e8334a467ff7be69578cd149dba3f0f6f2913
srisaiomkar <srisaiomkar@gmail.com>
2021-06-05 17:31:06 +0530

    delete and confirm delete

---
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -0,0 +1,12 @@
+﻿function ConfirmDelete(uniqueId, toDelete) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (toDelete) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
